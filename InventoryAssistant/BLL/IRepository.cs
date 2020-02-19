895d5d489ecb589308744a31bb6710e32f708e9f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 00:07:52 -0400

    IRepository Creado!

---
+++ InventoryAssistant/BLL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAssistant.BLL
+{
+    public interface IRepository<T> where T : class
+    {
+        bool Guardar(T entity);
+        bool Modificar(T entity);
+        bool Eliminar(int id);
+        T Buscar(int id);
+        List<T> GetList(Expression<Func<T, bool>> expression);
+    }
+}

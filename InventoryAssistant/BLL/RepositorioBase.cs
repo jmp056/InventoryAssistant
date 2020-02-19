e4707a6dd4efa4214ae6d6198dd0d29ada00af1e
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 00:13:55 -0400

    Contexto terminado con la entidad Usuario!

--- InventoryAssistant/BLL/RepositorioBase.cs
+++ InventoryAssistant/BLL/RepositorioBase.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using InventoryAssistant.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
---
+++ InventoryAssistant/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using InventoryAssistant.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAssistant.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        public Contexto() : base("ConStr")
+        {   }
+    }
+}

f0b7754ae45ed0e31bfd2cc1f9a772454c19e0d1
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 00:10:07 -0400

    Repositorio base creado

---
+++ InventoryAssistant/BLL/RepositorioBase.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAssistant.BLL
+{
+    public class RepositorioBase<T> : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto contexto;
+
+        public RepositorioBase()
+        {
+
+            contexto = new Contexto();
+        }
+
+        public virtual bool Guardar(T entity)
+        {
+
+            bool paso = false;
+
+            try
+            {
+
+                if (contexto.Set<T>().Add(entity) != null)
+                    paso = contexto.SaveChanges() > 0;
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            finally
+            {
+
+                Dispose();
+            }
+
+            return paso;
+
+        }
+
+
+        public virtual bool Modificar(T entity)
+        {
+
+            bool paso = false;
+
+            try
+            {
+
+                contexto.Entry(entity).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+
+            finally
+            {
+
+                Dispose();
+
+            }
+
+            return paso;
+        }
+
+
+        public virtual bool Eliminar(int id)
+        {
+
+            bool paso = false;
+
+            try
+            {
+
+                T entity = contexto.Set<T>().Find(id);
+                contexto.Set<T>().Remove(entity);
+                paso = contexto.SaveChanges() > 0;
+
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+
+            finally
+            {
+
+                Dispose();
+
+            }
+
+            return paso;
+        }
+
+
+        public virtual T Buscar(int id)
+        {
+
+            T entity;
+
+            try
+            {
+
+                entity = contexto.Set<T>().Find(id);
+
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+            finally
+            {
+
+                Dispose();
+
+            }
+
+            return entity;
+        }
+
+
+        public virtual List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+
+            List<T> Lista = new List<T>();
+
+            try
+            {
+
+                Lista = contexto.Set<T>().Where(expression).ToList();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+            finally
+            {
+
+                Dispose();
+
+            }
+
+            return Lista;
+
+        }
+
+        public void Dispose()
+        {
+
+            contexto.Dispose();
+
+        }
+
+    }
+}

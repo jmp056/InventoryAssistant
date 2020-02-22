cbec5b1e8a07f568bdd075f9e71c8b8e8e287415
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-22 13:43:44 -0400

    .

---
+++ InventoryAssistant/BLL/UsuariosBLL.cs
@@ -0,0 +1,33 @@
+﻿using InventoryAssistant.DAL;
+using InventoryAssistant.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAssistant.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool ExisteCedula(string cedula)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Usuarios.Any(p => p.Cedula.Equals(cedula)))
+                    paso = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+    }
+}
